0ee91234f4cb5349ebe685fabc17e733e5ff570f
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-07-04 15:59:34 +0900

    출석부 카드와 명단 모델 만들기

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Card, NameList
 
 # Register your models here.
+admin.site.register(Card)
+admin.site.register(NameList)
--- attendance/models.py
+++ attendance/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from common.models import User
 # Create your models here.
 class Card(models.Model):
-  pass
+  manager = models.ForeignKey(User, on_delete=models.CASCADE)
+  dt_created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+      return f"{self.manager} : {self.dt_created}"
+  
 
 
-class NameList(model.Model):
-  pass
+class NameList(models.Model):
+  name = models.CharField(max_length=10)
+
+  def __str__(self):
+      return self.name
+  

1b2ee99334269c12473930cdcfd8ac9fb4dbb1ae
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-07-17 16:38:57 +0900

    modify : update, delete in setting button, name_detail.html

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,22 +3,22 @@ from django.urls import path
 from .views import base_views, card_views, name_views
 
 urlpatterns = [
-  # base_views
-  path('', base_views.index, name='index'),
-  path('profile/', base_views.profile, name='profile'),
-  # card_views
-  path('list/', card_views.CardListView.as_view(), name='card_list'),
-  path('deatil/<int:pk>', card_views.CardDetailView.as_view(), name='card_detail'),
-  path('create/', card_views.CardCreateView.as_view(), name='card_create'),
-  path('update/<int:pk>', card_views.CardUpdateView.as_view(), name='card_update'),
-  path('delete/<int:pk>', card_views.CardDeleteView.as_view(), name='card_delete'),
-  # name_views
-  path('name/detail/<int:name_id>', name_views.NameDetailView.as_view(), name='name_detail'),
-  path('name/create/<int:pk>', name_views.name_create, name='name_create'),
-  # path('name/create/<int:pk>', name_views.NameCreateView.as_view(), name='name_create'),
-  path('name/update/<int:name_id>', name_views.name_update, name='name_update'),
-  path('name/delete/<int:name_id>', name_views.NameDeleteView.as_view(), name='name_delete'),
-  path('name/attend/<int:name_id>', name_views.name_attend, name='name_attend'),
-  path('name/late/<int:name_id>', name_views.name_late, name='name_late'),
-  path('name/absent/<int:name_id>', name_views.name_absent, name='name_absent'),
+    # base_views
+    path('', base_views.index, name='index'),
+    path('profile/', base_views.profile, name='profile'),
+    # card_views
+    path('list/', card_views.CardListView.as_view(), name='card_list'),
+    path('deatil/<int:pk>', card_views.CardDetailView.as_view(), name='card_detail'),
+    path('create/', card_views.CardCreateView.as_view(), name='card_create'),
+    path('update/<int:pk>', card_views.CardUpdateView.as_view(), name='card_update'),
+    path('delete/<int:pk>', card_views.CardDeleteView.as_view(), name='card_delete'),
+    # name_views
+    path('name/detail/<int:name_id>',
+         name_views.NameDetailView.as_view(), name='name_detail'),
+    path('name/create/<int:pk>', name_views.name_create, name='name_create'),
+    path('name/update/<int:name_id>', name_views.name_update, name='name_update'),
+    path('name/delete/<int:name_id>', name_views.name_delete, name='name_delete'),
+    path('name/attend/<int:name_id>', name_views.name_attend, name='name_attend'),
+    path('name/late/<int:name_id>', name_views.name_late, name='name_late'),
+    path('name/absent/<int:name_id>', name_views.name_absent, name='name_absent'),
 ]
--- attendance/views/name_views.py
+++ attendance/views/name_views.py
@@ -7,9 +7,10 @@ from django.views.generic import DetailView, CreateView, DeleteView
 from attendance.models import Card, Name
 from attendance.forms import NameForm
 
+
 class NameDetailView(DetailView):
-  model = Name
-  pk_url_kwarg = 'name_id'
+    model = Name
+    pk_url_kwarg = 'name_id'
 
 
 # class NameCreateView(LoginRequiredMixin, CreateView):
@@ -27,66 +28,65 @@ class NameDetailView(DetailView):
 
 @login_required(login_url='account_login')
 def name_create(request, pk):
-  card = get_object_or_404(Card, id=pk)
-  
-  if request.method == "POST":
-      form = NameForm(request.POST)
-      if form.is_valid():
-          name = form.save(commit=False)
-          name.card = card
-          name.save()
-          return redirect('card_detail', pk)
-  else:
-    form = NameForm()
+    card = get_object_or_404(Card, id=pk)
 
-  return render(request, 'attendance/name_form.html', {'form':form})
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.save(commit=False)
+            name.card = card
+            name.save()
+            return redirect('card_detail', pk)
+    else:
+        form = NameForm()
+
+    return render(request, 'attendance/name_form.html', {'form': form})
 
 
 @login_required(login_url='account_login')
 def name_update(request, name_id):
-  name = get_object_or_404(Name, id=name_id)
-  
-  if request.method == "POST":
-      form = NameForm(request.POST, instance=name)
-      if form.is_valid():
-          name.save()
-          return redirect('card_detail', pk=name.card.id)
-  else:
-      form = NameForm(instance=name)
-  ctx = {'form' : form}
-  return render(request, 'attendance/name_update_form.html', ctx)
+    name = get_object_or_404(Name, id=name_id)
+
+    if request.method == "POST":
+        form = NameForm(request.POST, instance=name)
+        if form.is_valid():
+            name.save()
+            return redirect('card_detail', pk=name.card.id)
+    else:
+        form = NameForm(instance=name)
+    ctx = {'form': form}
+    return render(request, 'attendance/name_update_form.html', ctx)
 
 
-class NameDeleteView(LoginRequiredMixin, DeleteView):
-  model = Name
-  pk_url_kwarg = 'name_id'
+@login_required(login_url='account_login')
+def name_delete(request, name_id):
+    name = get_object_or_404(Name, id=name_id)
+    name.delete()
+    return redirect('card_detail', pk=name.card.id)
 
-  def get_success_url(self):
-    return reverse('card_detail', kwargs={"pk":self.object.card.id})
 
-
-def name_attend(request,name_id):
-  name = get_object_or_404(Name, id=name_id)
-  name.dt_attend = timezone.now()
-  name.save()
-  return redirect('{}#name_{}'.format(
-    resolve_url('card_detail', pk=name.card.id),name.id))
+def name_attend(request, name_id):
+    name = get_object_or_404(Name, id=name_id)
+    name.dt_attend = timezone.now()
+    name.save()
+    return redirect('{}#name_{}'.format(
+        resolve_url('card_detail', pk=name.card.id), name.id))
 
 
 def name_late(request, name_id):
-  name = get_object_or_404(Name, id=name_id)
-  name.dt_attend = None
-  name.dt_late = timezone.now()
-  name.save()
-  return redirect('{}#name_{}'.format(
-    resolve_url('card_detail', pk=name.card.id),name.id))
+    name = get_object_or_404(Name, id=name_id)
+    name.dt_attend = None
+    name.dt_late = timezone.now()
+    name.save()
+    return redirect('{}#name_{}'.format(
+        resolve_url('card_detail', pk=name.card.id), name.id))
 
 
 def name_absent(request, name_id):
-  name = get_object_or_404(Name, id=name_id)
-  name.dt_attend = None
-  name.dt_late = None
-  name.dt_absent = timezone.now()
-  name.save()
-  return redirect('{}#name_{}'.format(
-    resolve_url('card_detail', pk=name.card.id),name.id))
+    name = get_object_or_404(Name, id=name_id)
+    name.dt_attend = None
+    name.dt_late = None
+    name.dt_absent = timezone.now()
+    name.save()
+    return redirect('{}#name_{}'.format(
+        resolve_url('card_detail', pk=name.card.id), name.id))

1d769d4ef4426f1ab1246d05c76487b6e9318b20
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-07-18 13:40:08 +0900

    modify : settings.py, base,footer.html, card_confirm_delete.html

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.kakao",
     'bootstrap5',
     'fontawesomefree',
 ]

7ee7310dd90f1aa765137f3bfab397924d454519
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-08-01 22:21:51 +0900

    delete : bookmark 기능 삭제, bpresent 기능의 목적 재설정 필요함

--- attendance/views/bookmark_views.py
+++ attendance/views/bookmark_views.py
@@ -6,22 +6,27 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from attendance.forms import BookmarkForm
 
 
-@login_required(login_url='account_login')
-def bookmark_create(request, pk):
-    card = get_object_or_404(Card, id=pk)
-    if request.method == 'POST':
-        form = BookmarkForm(request.POST)
-        if form.is_valid():
-            bookmark = form.save(commit=False)
-            bookmark.card = card
-            bookmark.save()
-            return redirect('profile')
-    else:
-        form = BookmarkForm()
-    ctx = {'form': form, 'card': card}
-    return render(request, 'attendance/bookmark_form.html', ctx)
+# @login_required(login_url='account_login')
+# def bookmark_create(request, pk):
+#     card = get_object_or_404(Card, id=pk)
+#     bookmark = get_object_or_404(Bookmark, id=pk)
+
+#     if bookmark in card.bookmark.all:
+#         print('이미 즐겨찾기가 있습니다.')
+
+#     if request.method == 'POST':
+#         form = BookmarkForm(request.POST)
+#         if form.is_valid():
+#             bookmark = form.save(commit=False)
+#             bookmark.card = card
+#             bookmark.save()
+#             return redirect('profile')
+#     else:
+#         form = BookmarkForm()
+#     ctx = {'form': form, 'card': card}
+#     return render(request, 'attendance/bookmark_form.html', ctx)
 
 
-@login_required(login_url='account_login')
-def bookmark_update(request, pk):
-    pass
+# @login_required(login_url='account_login')
+# def bookmark_update(request, pk):
+#     pass
